122142754a7a5fbb493cdbc2faa67dfad2952d8e
Rıdvan Demirci <ridvandemirci@192.168.1.75>
2023-08-24 16:56:31 +0300

    feat:support theme dark, light and core

---
+++ src/utils/craete-theme.ts
@@ -0,0 +1,80 @@
+import fs from "fs";
+
+import { ITheme, TargetType, TokenType } from "../types";
+import { execute } from "./execute";
+import { flatObj } from "./flatten";
+import { selfReferencObject } from "./self-reference";
+
+const cssTemplate = (target: string, token: TokenType, isRoot = false) =>
+  `${isRoot ? `:root ` : `html [theme='${target}']`} {\n${Object.entries(token)
+    .map(([key, value]) => `${key}: ${value};\n`)
+    .join("")}}`;
+
+const scssTemplate = (target: string, data: TokenType) => {
+  return `$${target}:(\n${Object.entries(data)
+    .map(([key, value]) => `\t${key}: ${value},\n`)
+    .join("")});\n`;
+};
+
+export const createTheme = ({
+  darkTokens,
+  lightTokens,
+  radiusTokens,
+  shadowTokens,
+  spacingTokens,
+}: ITheme) => {
+  const flatAndRefence = (obj: Object) => flatObj(selfReferencObject(obj));
+  const executeObj = (obj: Object, type: TargetType = "css", prefix = "--") =>
+    execute({
+      obj: flatAndRefence(obj),
+      targetCase: "kebab-case",
+      type: type,
+      delimiter: "-",
+      prefix: prefix,
+      fileName: "",
+    });
+
+  const toCss = (fileName: string) => {
+    const dark = executeObj(darkTokens);
+    const light = executeObj(lightTokens);
+    const radius = executeObj(radiusTokens) as Object;
+    const shadow = executeObj(shadowTokens) as Object;
+    const spacing = executeObj(spacingTokens) as Object;
+
+    const root = `${cssTemplate(
+      "",
+      {
+        ...radius,
+        ...shadow,
+        ...spacing,
+      },
+      true
+    )}\n`;
+
+    const themeColor = `${cssTemplate("dark", dark)} \n\n  ${cssTemplate(
+      "light",
+      light
+    )}`;
+
+    fs.writeFile(fileName, root + themeColor, (err) => console.error(err));
+  };
+
+  const toScss = (fileName: string) => {
+    const dark = scssTemplate("dark", executeObj(darkTokens, "scss", "$"));
+    const light = scssTemplate("light", executeObj(lightTokens, "scss", "$"));
+    const core = scssTemplate(
+      "core",
+      executeObj(
+        { ...radiusTokens, ...shadowTokens, ...spacingTokens },
+        "scss",
+        "$"
+      )
+    );
+
+    const scssToken = `${dark}\n${light}\n${core}`;
+
+    fs.writeFile(fileName, scssToken, (err) => console.error(err));
+  };
+
+  return { toCss, toScss };
+};
---
+++ test/data.ts
@@ -0,0 +1,80 @@
+export const defaultLightBrand = {
+  test: {
+    blue: {
+      900: "#03045E",
+      800: "#023E8A",
+      700: "#0077B6",
+      600: "#0096C7",
+      500: "#00B4D8",
+      400: "#48CAE4",
+      300: "#90E0EF",
+      200: "#ADE8F4",
+      100: "#CAF0F8",
+    },
+    red: {
+      900: "#590D22",
+      800: "#800F2F",
+      700: "#A4133C",
+      600: "#C9184A",
+      500: "#FF4D6D",
+      400: "#FF758F",
+      300: "#FF8FA3",
+      200: "#FFB3C1",
+      100: "#FFCCD5",
+      50: "#FFF0F3",
+    },
+    yellow: {
+      900: "#FF7B00",
+      800: "#FF8800",
+      700: "#FF9500",
+      600: "#FFA200",
+      500: "#FFAA00",
+      400: "#FFB700",
+      300: "#FFC300",
+      200: "#FFD000",
+      100: "#FFDD00",
+      50: "#FFEA00",
+    },
+    gray: {
+      900: "#212529",
+      800: "#343A40",
+      700: "#495057",
+      600: "#6C757D",
+      500: "#ADB5BD",
+      400: "#CED4DA",
+      300: "#DEE2E6",
+      200: "#E9ECEF",
+      100: "#F8F9FA",
+    },
+    white: "#ffffff",
+    background: "${this.test.gray[100]}",
+    primaryButtonBackground: "${this.test.blue[600]}",
+    primaryButtonColor: "${this.test.white}",
+    secondaryButtonBackground: "${this.test.white}",
+    secondaryButtonColor: "${this.test.white}",
+  },
+};
+
+export const radius = {
+  border: {
+    1: "4px",
+    2: "8px",
+    3: "12px",
+    4: "16px",
+  },
+};
+
+export const spacing = {
+  space: {
+    1: "12px",
+    2: "16px",
+    3: "24px",
+  },
+};
+
+export const shadow = {
+  shadow: {
+    1: "0 0 0 1px gray",
+    2: "0 0 0 2px red",
+  },
+};
